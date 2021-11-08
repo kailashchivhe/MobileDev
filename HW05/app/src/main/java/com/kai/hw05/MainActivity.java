a5e627c607fa8049291e425030560174987be20e
Kailash Chivhe <kchivhe@gmail.com>
2021-11-08 17:00:34 -0500

    Comments HW05

--- HW05/app/src/main/java/com/kai/hw05/MainActivity.java
+++ HW05/app/src/main/java/com/kai/hw05/MainActivity.java
@@ -6,6 +6,11 @@ import android.os.Bundle;
 
 import com.kai.hw05.firebase.FirebaseHelper;
 
+/*
+* Name: Kailash Chivhe & Ankit Vaity
+* Assignment: HW05
+* MainActivity
+* */
 public class MainActivity extends AppCompatActivity{
 
     @Override
