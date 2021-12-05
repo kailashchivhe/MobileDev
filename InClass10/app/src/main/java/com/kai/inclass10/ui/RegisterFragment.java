3c95ce143c637f434accdfdaaa035d448ff91070
Ankit Vaity <30876031+AnkitVaity@users.noreply.github.com>
2021-12-05 15:40:33 -0500

    Register Changes

--- InClass10/app/src/main/java/com/kai/inclass10/ui/RegisterFragment.java
+++ InClass10/app/src/main/java/com/kai/inclass10/ui/RegisterFragment.java
@@ -1,17 +1,26 @@
 package com.kai.inclass10.ui;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.kai.inclass10.R;
 import com.kai.inclass10.databinding.FragmentRegisterBinding;
+import com.kai.inclass10.listener.RegisterListener;
+import com.kai.inclass10.sdk.FirebaseHelper;
 
-public class RegisterFragment extends Fragment {
+public class RegisterFragment extends Fragment implements RegisterListener {
 
     FragmentRegisterBinding fragmentRegisterBinding;
 
@@ -25,7 +34,33 @@ public class RegisterFragment extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
+        fragmentRegisterBinding.cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NavHostFragment.findNavController(RegisterFragment.this)
+                        .navigate(R.id.action_registerFragment_to_LoginFragment);
+            }
+        });
+
+        fragmentRegisterBinding.submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onSubmitClick();
+            }
+        });
+    }
+
+    void onSubmitClick() {
+        if( !fragmentRegisterBinding.editTextEmail.getText().toString().isEmpty() && !fragmentRegisterBinding.editTextPassword.getText().toString().isEmpty()) {
+            FirebaseHelper.create(fragmentRegisterBinding.editTextEmail.getText().toString(), fragmentRegisterBinding.editTextPassword.getText().toString(), this);
+        } else  {
+            showFailureMessage(getString(R.string.credentials_invalid));
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -33,5 +68,26 @@ public class RegisterFragment extends Fragment {
         // Inflate the layout for this fragment
         fragmentRegisterBinding = FragmentRegisterBinding.inflate( inflater, container, false);
         return fragmentRegisterBinding.getRoot();
+    }
+
+    @Override
+    public void onSuccess() {
+        NavHostFragment.findNavController(RegisterFragment.this)
+                .navigate(R.id.action_registerFragment_to_HomeFragment);
+    }
+
+    @Override
+    public void onFailure(String message) {
+        showFailureMessage( message );
+    }
+
+    private void showFailureMessage(String message) {
+        new Handler( Looper.getMainLooper()).post(() -> {
+            AlertDialog.Builder builder = new AlertDialog.Builder( getContext() );
+            builder.setTitle( R.string.failure );
+            builder.setMessage( message );
+            builder.setCancelable( true );
+            builder.show();
+        });
     }
 }
