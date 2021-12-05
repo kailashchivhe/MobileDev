4b1a92ca2048a15f2e860001329de0f40e442714
Kailash Chivhe <kchivhe@gmail.com>
2021-12-04 22:47:14 -0500

    Updated MainActivity as launcher and RegisterFragment

--- InClass10/app/src/main/java/com/kai/inclass10/MainActivity.java
+++ InClass10/app/src/main/java/com/kai/inclass10/MainActivity.java
@@ -1,13 +1,7 @@
 package com.kai.inclass10;
 
 import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.View;
-
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
